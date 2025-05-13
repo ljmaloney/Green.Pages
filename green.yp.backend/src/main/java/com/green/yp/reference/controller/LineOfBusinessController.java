b9f407a7db10a076408666c20ff78deaaf6e4244
ljmaloney <maloney1@mindspring.com>
2025-05-13 17:40:32 -0400

    add dockerfile for fusionauth using mysql

--- green.yp.backend/src/main/java/com/green/yp/reference/controller/LineOfBusinessController.java
+++ green.yp.backend/src/main/java/com/green/yp/reference/controller/LineOfBusinessController.java
@@ -11,6 +11,10 @@ import com.green.yp.reference.service.LineOfBusinessService;
 import com.green.yp.util.RequestUtil;
 import java.util.List;
 import java.util.UUID;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Validated
 @RequestMapping("reference")
+@Tag(name = "REST endpoint to manage supported lines of business")
 public class LineOfBusinessController extends BaseRestController {
 
   private final LineOfBusinessService lobService;
@@ -32,17 +37,26 @@ public class LineOfBusinessController extends BaseRestController {
     this.lobMapper = lobMapper;
   }
 
+  @Operation(summary = "Returns a list of the active lines of business")
+  @ApiResponse(responseCode = "200")
+  @ApiResponse(responseCode = "404", description = "No lines of business configured / active")
   @GetMapping(value = "/lob", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseApi<List<LineOfBusinessDto>> getLineOfBusiness() {
     return new ResponseApi<>(lobMapper.toApi(lobService.getAllLineOfBusiness()), null);
   }
 
+  @Operation(summary = "Returns a list of the services offered by a line of business")
+  @ApiResponse(responseCode = "200")
+  @ApiResponse(responseCode = "404", description = "No services found for a line of business")
   @GetMapping(value = "/lob/{lineOfBusiness}/service", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseApi<List<LOBServiceDto>> findService(
       @PathVariable("lineOfBusiness") UUID lineOfBusinessId) {
     return new ResponseApi<>(lobService.findServices(lineOfBusinessId), null);
   }
 
+  @Operation(summary = "Creates a new line of business")
+  @ApiResponse(responseCode = "200", description = "Line of business created")
+  @ApiResponse(responseCode = "412", description = "Precondition for line of business creation failed")
   @PostMapping(
       path = "/lob",
       consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -53,6 +67,9 @@ public class LineOfBusinessController extends BaseRestController {
         lobService.createLineOfBusiness(lineOfBusiness, null, RequestUtil.getRequestIP()), null);
   }
 
+  @Operation(summary = "Creates a new service of a line of business")
+  @ApiResponse(responseCode = "200", description = "New service added to a line of business")
+  @ApiResponse(responseCode = "404", description = "Line of business for service not found")
   @PostMapping(
       value = "/lob/service",
       consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -63,6 +80,8 @@ public class LineOfBusinessController extends BaseRestController {
         lobService.createService(serviceRequest, null, RequestUtil.getRequestIP()), null);
   }
 
+  @Operation(summary = "Updates a description for a line of business")
+  @ApiResponse(responseCode = "200")
   @PutMapping(
       path = "/lob/description",
       consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -72,6 +91,7 @@ public class LineOfBusinessController extends BaseRestController {
         lobService.updateLineOfBusiness(lobDto, null, RequestUtil.getRequestIP()), null);
   }
 
+  @Operation(summary = "Updates a line of business")
   @PutMapping(
       value = "/lob/service",
       consumes = MediaType.APPLICATION_JSON_VALUE,
