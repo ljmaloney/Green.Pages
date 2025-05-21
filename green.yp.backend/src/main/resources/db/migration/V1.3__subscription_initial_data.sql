194cfd4cb2a582121b4087db631996f542c1e4a1
ljmaloney <maloney1@mindspring.com>
2025-05-21 18:20:27 -0400

    implement flyway to support intial config any any migrations

---
+++ green.yp.backend/src/main/resources/db/migration/V1.3__subscription_initial_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO greenyp.subscription
+    (id, version, create_date, last_update_date,
+     line_of_business_id, subscription_type, display_name, sort_order,
+     start_date, end_date, monthly_autopay_amount, quarterly_autopay_amount,
+     annual_bill_amount, short_description, html_description)
+VALUES (0xCA68C861B985421295ADBF00B22FF50C, 0, '2024-05-20 22:48:02', '2024-05-20 22:48:02',
+        null, 'TOP_LEVEL', 'Basic Subscription', 0,
+        '2023-08-31', '9999-12-30', 30.00, 100.00,
+        300.00, 'Adds information request form', 'Basic Subscription');
