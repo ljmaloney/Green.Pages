ebc6c127a52a1df6b83c91a7862698a1808a7684
ljmaloney <maloney1@mindspring.com>
2025-06-09 17:57:10 -0400

    CORS temp fix for dev purposes

--- green.yp.backend/src/main/java/com/green/yp/config/CorsConfig.java
+++ green.yp.backend/src/main/java/com/green/yp/config/CorsConfig.java
@@ -9,23 +9,40 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/reference/**")
-                .allowedOrigins("http://localhost:8080","http://services.greenyp.com","https://services.greenyp.com")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true);
+    registry
+        .addMapping("/reference/**")
+        .allowedOrigins(
+            "http://localhost:8080",
+            "http://services.greenyp.com",
+            "https://services.greenyp.com",
+            "https://*.lovable.app")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("Content-Type", "Authorization")
+        .allowCredentials(true);
         registry.addMapping("/account/**")
-                .allowedOrigins("http://localhost:8080","http://services.greenyp.com","https://services.greenyp.com")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://services.greenyp.com",
+                        "https://services.greenyp.com",
+                        "https://*.lovable.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
         registry.addMapping("/producer/**")
-                .allowedOrigins("http://localhost:8080","http://services.greenyp.com","https://services.greenyp.com")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://services.greenyp.com",
+                        "https://services.greenyp.com",
+                        "https://*.lovable.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
         registry.addMapping("/search/**")
-                .allowedOrigins("http://localhost:8080","http://services.greenyp.com","https://services.greenyp.com")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://services.greenyp.com",
+                        "https://services.greenyp.com",
+                        "https://*.lovable.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
