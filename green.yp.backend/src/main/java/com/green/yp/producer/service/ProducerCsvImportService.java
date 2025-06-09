425427dc4268f53b5bb8a892c10a908f9ed5efeb
ljmaloney <maloney1@mindspring.com>
2025-06-09 17:02:06 -0400

    refactor to move creation code into object under @Transactional

--- green.yp.backend/src/main/java/com/green/yp/producer/service/ProducerCsvImportService.java
+++ green.yp.backend/src/main/java/com/green/yp/producer/service/ProducerCsvImportService.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ForkJoinPool;
+
+import com.green.yp.producer.service.dataimp.ProducerImportService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
@@ -27,11 +29,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RequiredArgsConstructor
 public class ProducerCsvImportService {
-  private static final String IMPORT_SUB_ID = "e0315cb5-a2e2-40e1-abb2-4ce646439730";
-  private final ProducerOrchestrationService producerOrchestrationService;
-  private final ProducerLocationService producerLocationService;
-  private final ProducerContactOrchestrationService contactOrchestrationService;
-  private final GeocodingService geocodingService;
+
+  private final ProducerImportService importService;
 
   @Value("${green.yp.import.threads:5}")
   private int importThreads;
@@ -51,7 +50,7 @@ public class ProducerCsvImportService {
       customThreadPool.submit(() ->
       records.parallelStream()
           .filter(csvRecord -> csvRecord.getRecordNumber() != 1)
-              .map(csvRecord -> new ProducerCsvRecord(
+              .map(csvRecord -> new ProducerImportService.ProducerCsvRecord(
                       csvRecord.getRecordNumber(),
                       csvRecord.get(0),
                       csvRecord.get(1),
@@ -64,65 +63,10 @@ public class ProducerCsvImportService {
                       csvRecord.get(8),
                       StringUtils.truncate(csvRecord.get(9), 12)))
               .forEach( parsedRecord -> {
-                  try {
-                    var location = geocodingService.getCoordinates(parsedRecord.address, parsedRecord.city, parsedRecord.state, parsedRecord.zip);
-
-                  // Use contact as company name if company is empty
-                  String businessName =
-                      StringUtils.getIfBlank(parsedRecord.company, () -> parsedRecord.contact);
-                  ProducerResponse producerResponse =
-                      producerOrchestrationService.createProducer(
-                          new CreateProducerRequest(
-                              businessName,
-                              lineOfBusinessId,
-                              UUID.fromString(IMPORT_SUB_ID),
-                              ProducerSubscriptionType.LIVE_UNPAID,
-                              null,
-                              null,
-                              null),
-                          null);
-
-                  synchronized (createdProducerIds) {
-                    createdProducerIds.add(producerResponse.producerId());
-                  }
-
-                  ProducerLocationResponse locationResponse =
-                      producerLocationService.createLocation(
-                          new LocationRequest(
-                              null,
-                              businessName,
-                              ProducerLocationType.HOME_OFFICE_PRIMARY,
-                              LocationDisplayType.CITY_STATE_ZIP,
-                              true,
-                              parsedRecord.address,
-                              null,
-                              null,
-                              parsedRecord.city,
-                              parsedRecord.state,
-                              parsedRecord.zip,
-                              location.latitude(),
-                              location.longitude(),
-                              null),
-                          producerResponse.producerId(),
-                          null);
-
-                  contactOrchestrationService.createContact(
-                      new ProducerContactRequest(
-                          null,
-                          locationResponse.locationId(),
-                          ProducerContactType.PRIMARY,
-                          ProducerDisplayContactType.GENERIC_NAME_PHONE_EMAIL,
-                          StringUtils.getIfBlank(parsedRecord.contact, () -> "Primary"),
-                          null,
-                          null,
-                          parsedRecord.phone,
-                          null,
-                          null),
-                      Optional.empty(),
-                      producerResponse.producerId(),
-                      locationResponse.locationId(),
-                      null);
-                  log.info("Imported : {}", parsedRecord);
+                  try{
+                    UUID createdProducerId = importService.importProducer(lineOfBusinessId, parsedRecord);
+                    createdProducerIds.add(createdProducerId);
+                    log.info("Imported : {}", parsedRecord);
                 } catch (Exception e) {
                   log.error("Error processing {}", parsedRecord, e);
                   throw new SystemException(
@@ -138,16 +82,4 @@ public class ProducerCsvImportService {
     }
     return createdProducerIds;
   }
-    record ProducerCsvRecord(
-            long recordNumber,
-            String type,
-            String licenseNumber,
-            String contact,
-            String company,
-            String address,
-            String city,
-            String state,
-            String zip,
-            String county,
-            String phone) {}
 }
