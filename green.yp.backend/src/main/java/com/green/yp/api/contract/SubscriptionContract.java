0d9e827d26e659c35fa90cb525078b718a3c029a
ljmaloney <maloney1@mindspring.com>
2025-06-20 14:18:19 -0400

    add code to validate producer has correct subscription feature to upload logo and gallery

--- green.yp.backend/src/main/java/com/green/yp/api/contract/SubscriptionContract.java
+++ green.yp.backend/src/main/java/com/green/yp/api/contract/SubscriptionContract.java
@@ -1,8 +1,12 @@
 package com.green.yp.api.contract;
 
+import com.green.yp.reference.data.enumeration.SubscriptionType;
 import com.green.yp.reference.dto.SubscriptionDto;
 import com.green.yp.reference.service.SubscriptionService;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import lombok.NonNull;
 import org.springframework.stereotype.Component;
@@ -18,5 +22,14 @@ public class SubscriptionContract {
 
   public SubscriptionDto findSubscription(@NotNull @NonNull UUID subscriptionId) {
     return subscriptionService.findActiveSubscription(subscriptionId);
+  }
+
+  public List<SubscriptionDto> getAllSubscriptions(SubscriptionType subscriptionType, boolean active) {
+    var today = new Date();
+    return subscriptionService.findActiveSubscription()
+            .stream()
+            .filter( sub -> sub.subscriptionType() == subscriptionType)
+            .filter( sub -> !active || ( active && sub.startDate().before(today) && sub.endDate().after(today)) )
+            .toList();
   }
 }
