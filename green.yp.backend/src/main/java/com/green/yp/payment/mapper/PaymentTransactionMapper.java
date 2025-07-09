252042b5c76059deb8f8fe4cad33752b60085be3
ljmaloney <maloney1@mindspring.com>
2025-07-09 18:31:30 -0400

    return data from transaction service

--- green.yp.backend/src/main/java/com/green/yp/payment/mapper/PaymentTransactionMapper.java
+++ green.yp.backend/src/main/java/com/green/yp/payment/mapper/PaymentTransactionMapper.java
@@ -5,6 +5,7 @@ import com.green.yp.api.apitype.payment.PaymentTransactionResponse;
 import com.green.yp.payment.data.model.PaymentTransaction;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
@@ -15,5 +16,6 @@ public interface PaymentTransactionMapper {
 
     PaymentTransaction toEntity(PaymentRequest paymentRequest);
 
+    @Mapping(target="transactionId", source="id")
     PaymentTransactionResponse fromEntity(PaymentTransaction save);
 }
