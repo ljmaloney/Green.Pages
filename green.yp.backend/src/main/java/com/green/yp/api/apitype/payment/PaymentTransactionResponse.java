518b7a48cb511282ec2e05dfe496937b366ec4ba
ljmaloney <maloney1@mindspring.com>
2025-07-09 18:27:56 -0400

    return data from transaction service

--- green.yp.backend/src/main/java/com/green/yp/api/apitype/payment/PaymentTransactionResponse.java
+++ green.yp.backend/src/main/java/com/green/yp/api/apitype/payment/PaymentTransactionResponse.java
@@ -1,3 +1,26 @@
 package com.green.yp.api.apitype.payment;
 
-public record PaymentTransactionResponse() {}
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record PaymentTransactionResponse(UUID transactionId,
+                                         String paymentRef,
+                                         String locationRef,
+                                         String orderRef,
+                                         String customerRef,
+                                         String receiptNumber,
+                                         String ipAddress,
+                                         String status,
+                                         String sourceType,
+                                         BigDecimal amount,
+                                         BigDecimal refundAmount,
+                                         BigDecimal totalAmount,
+                                         String currencyCode,
+                                         String firstName,
+                                         String lastName,String address,String city,
+                                         String state,
+                                         String postalCode,
+                                         String phoneNumber,
+                                         String emailAddress,
+                                         String statementDescriptionIdentifier,
+                                         String receiptUrl){}
