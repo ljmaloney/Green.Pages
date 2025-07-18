d837e51a146a9ee296598824f5a60ec0806b914b
ljmaloney <maloney1@mindspring.com>
2025-07-18 13:24:54 -0400

    add log messages

--- green.yp.backend/src/main/java/com/green/yp/config/security/WebUserConfig.java
+++ green.yp.backend/src/main/java/com/green/yp/config/security/WebUserConfig.java
@@ -1,20 +1,25 @@
 package com.green.yp.config.security;
 
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebUserConfig implements WebMvcConfigurer {
   private final AuthenticatedUserResolver resolver;
 
   public WebUserConfig(AuthenticatedUserResolver resolver) {
+    log.debug("AuthenticatedUserResolver invoked");
     this.resolver = resolver;
   }
 
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    log.debug("AuthenticatedUserResolver added");
     resolvers.add(resolver);
   }
 }
