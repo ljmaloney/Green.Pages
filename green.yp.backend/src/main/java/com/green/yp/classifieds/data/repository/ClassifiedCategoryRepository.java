d81e86e962aaf88708b79be394380ec6c276c948
ljmaloney <maloney1@mindspring.com>
2025-08-04 17:43:52 -0400

    add parameter for category url name to mostRecent endpoint

--- green.yp.backend/src/main/java/com/green/yp/classifieds/data/repository/ClassifiedCategoryRepository.java
+++ green.yp.backend/src/main/java/com/green/yp/classifieds/data/repository/ClassifiedCategoryRepository.java
@@ -1,7 +1,11 @@
 package com.green.yp.classifieds.data.repository;
 
 import com.green.yp.classifieds.data.model.ClassifiedCategory;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassifiedCategoryRepository extends JpaRepository<ClassifiedCategory, UUID> {}
+public interface ClassifiedCategoryRepository extends JpaRepository<ClassifiedCategory, UUID> {
+    Optional<ClassifiedCategory> findByUrlName(String categoryName);
+}
