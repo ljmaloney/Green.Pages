c8e7fd157cd8cd47815f96c1be0f521ab81acb5b
ljmaloney <maloney1@mindspring.com>
2025-08-03 20:41:03 -0400

    add lobUrl parameter to support url or lob id

--- green.yp.backend/src/main/java/com/green/yp/producer/service/ProducerProfileService.java
+++ green.yp.backend/src/main/java/com/green/yp/producer/service/ProducerProfileService.java
@@ -4,11 +4,15 @@ import com.green.yp.api.apitype.producer.ProducerProfileResponse;
 import com.green.yp.api.apitype.search.TruncatedProducerResponse;
 import com.green.yp.api.contract.LineOfBusinessContract;
 import com.green.yp.exception.NotFoundException;
+import com.green.yp.exception.PreconditionFailedException;
 import com.green.yp.producer.data.repository.ProducerSearchRepository;
 import com.green.yp.producer.mapper.ProducerSearchMapper;
 import java.util.List;
 import java.util.UUID;
+
+import com.green.yp.reference.dto.LineOfBusinessDto;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
 
@@ -41,15 +45,26 @@ public class ProducerProfileService {
   }
 
   public List<TruncatedProducerResponse> getProfiles(
-      UUID lineOfBusinessId, Boolean mostRecent, Integer maxLimit) {
+          UUID lineOfBusinessId, String lobUrl, Boolean mostRecent, Integer maxLimit) {
     log.info(
-        "Searching for producers with lineOfBusinessId: {}, limit: {}", lineOfBusinessId, maxLimit);
+        "Searching for producers with lineOfBusinessId: {}, limit: {}",
+            StringUtils.isBlank(lobUrl) ? lineOfBusinessId : lobUrl, maxLimit);
 
-    lineOfBusinessContract.findLineOfBusiness(lineOfBusinessId);
+    if (StringUtils.isBlank(lobUrl) && lineOfBusinessId == null ) {
+      log.warn("Missing required parameter, either lobUrl or lineOfBusinessId is required");
+      throw new PreconditionFailedException("Missing required parameter, either lobUrl or lineOfBusinessId is required");
+    }
 
-    var results = searchRepository.findMostRecentProfiles(lineOfBusinessId, Limit.of(maxLimit));
+    LineOfBusinessDto lobDto = null;
+    if ( StringUtils.isNotBlank(lobUrl) ) {
+      lobDto = lineOfBusinessContract.findLineOfBusiness(lobUrl);
+    } else {
+      lobDto = lineOfBusinessContract.findLineOfBusiness(lineOfBusinessId);
+    }
 
-    log.info("Found {} producers for lineOfBusinessId: {}", results.size(), lineOfBusinessId);
+    var results = searchRepository.findMostRecentProfiles(lobDto.lineOfBusinessId(), Limit.of(maxLimit));
+
+    log.info("Found {} producers for lineOfBusinessId: {}", results.size(), lobDto.lineOfBusinessId());
 
     return producerSearchMapper.limitedOutputResponse(results);
   }
