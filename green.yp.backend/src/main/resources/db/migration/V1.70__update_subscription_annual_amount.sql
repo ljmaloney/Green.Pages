054f5c12ef6a9af166cfdfba5a102e90d16d5da8
ljmaloney <maloney1@mindspring.com>
2025-08-19 14:26:01 -0400

    update annual bill amount

--- green.yp.backend/src/main/resources/db/migration/V1.70__update_subscription_annual_amount.sql
+++ green.yp.backend/src/main/resources/db/migration/V1.70__update_subscription_annual_amount.sql
@@ -2,5 +2,5 @@ update subscription
 set annual_bill_amount = 12 * monthly_autopay_amount,
     quarterly_autopay_amount = 3 * monthly_autopay_amount
 where
-    end_date != '9999-12-31'
+    end_date = '9999-12-31'
     and subscription_type != 'DATA_IMPORT_NO_DISPLAY'
